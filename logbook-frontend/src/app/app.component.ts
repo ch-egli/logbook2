40d162423abfc8386332251b7e4b5dd62daba137
ch-egli <chrsitian.egli@gmx.net>
2019-05-12 00:03:14 +0200

    add pwa support

--- logbook-frontend/src/app/app.component.ts
+++ logbook-frontend/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    constructor() {
+    constructor(private swUpdate: SwUpdate) {
+    }
+
+    ngOnInit() {
+        if (this.swUpdate.isEnabled) {
+            this.swUpdate.available.subscribe(() => {
+                if (confirm('Eine neue Version von Logbook ist verfügbar. Willst du sie laden?')) {
+
+                    window.location.reload();
+                }
+            });
+        }
     }
 }
