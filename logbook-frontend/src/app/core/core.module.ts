533bb58dc2ba2d0c9e6e93a30326fecbbb2e381a
ch-egli <chrsitian.egli@gmx.net>
2019-05-11 21:43:43 +0200

    cleanup

--- logbook-frontend/src/app/core/core.module.ts
+++ logbook-frontend/src/app/core/core.module.ts
@@ -1,12 +1,3 @@
-/**
- * Copyright (C) Schweizerische Bundesbahnen SBB, 2017.
- *
- * ESTA WebJS: Core Module
- *
- * @author u218609 (Kevin Kreuzer)
- * @version: 2.0.0
- * @since 28.04.2017, 2017.
- */
 import {CommonModule} from '@angular/common';
 import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {RouterModule} from '@angular/router';
@@ -15,28 +6,15 @@ import {throwIfAlreadyLoaded} from './module-import-guard';
 import {NavComponent} from './nav/nav.component';
 import {AUTH_INTERCEPTOR, AuthModule} from 'esta-webjs-extensions';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {environment} from '../../environments/environment';
 
 // AOT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
-}
-
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
         AuthModule.forRoot(environment.authConfig, environment.authOptions),
-        HttpClientModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }),
+        HttpClientModule
     ],
     declarations: [NavComponent],
     exports: [NavComponent],
