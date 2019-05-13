6b2f3695eeab17a5a06e2128b3240746ffe6f5dc
ch-egli <chrsitian.egli@gmx.net>
2019-05-13 10:09:43 +0200

    add certificate and rename...

--- logbook-frontend/src/app/logbook/charts/chart.service.ts
+++ logbook-frontend/src/app/logbook/charts/chart.service.ts
@@ -6,6 +6,8 @@ import { ChartMetaType } from './chart.model';
 @Injectable()
 export class ChartService {
     readonly ENDPOINT_URL_LOCAL = 'http://192.168.1.121:8080/v1/ng2-charts/zoe/workouts/2019';
+    // readonly ENDPOINT_URL_LOCAL = 'https://192.168.1.121:8443/v1/ng2-charts/zoe/workouts/2019';
+
 
     constructor(private http: HttpClient) {
     }
