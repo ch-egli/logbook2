419f7dd5d94e34304dd98b0e7b030f1e96777a52
ch-egli <chrsitian.egli@gmx.net>
2019-06-07 17:40:07 +0200

    basic user filtering

--- logbook-frontend/src/app/logbook/home/home.component.ts
+++ logbook-frontend/src/app/logbook/home/home.component.ts
@@ -3,6 +3,7 @@ import { Workout, WorkoutPageable } from '../workouts/workout.model';
 import { Observable } from 'rxjs';
 import { WorkoutService } from '../workouts/workout.service';
 import { LazyLoadEvent } from 'primeng/primeng';
+import { SelectItem } from 'primeng/api';
 
 import { AuthenticationService } from '../../core/_services/authentication.service';
 
@@ -19,6 +20,12 @@ export class HomeComponent implements OnInit {
   userDiscriminator: string;
   totalWorkouts: number;
   workoutPageSize = 10;
+
+  benutzerOptions: SelectItem[] = [
+    { label: null, value: null },
+    { label: 'zoe', value: 'zoe' },
+    { label: 'liv', value: 'liv' },
+  ];
 
   workouts: Workout[];
   pagedWorkoutObservable: Observable<WorkoutPageable>;
@@ -50,6 +57,25 @@ export class HomeComponent implements OnInit {
       this.workouts = res.content;
       this.totalWorkouts = res.totalElements;
     });
+  }
+
+  public onBenutzerChange(event) {
+    if (event.value) {
+      this.userDiscriminator = event.value;
+    } else {
+      this.userDiscriminator = 'all';
+    }
+
+    this.pagedWorkoutObservable = this.workoutService.getPagedWorkouts(this.userDiscriminator, 0, this.workoutPageSize);
+    this.pagedWorkoutObservable.subscribe((res) => {
+      console.log(res);
+      this.workouts = res.content;
+      this.totalWorkouts = res.totalElements;
+    });
+  }
+
+  public isTrainer() {
+    return this.authenticationService.isTrainer();
   }
 
   private getUserDiscriminator() {
