80bf61abce534333105238a5349b430fb4fd9dfd
ch-egli <chrsitian.egli@gmx.net>
2019-06-16 23:16:08 +0200

    set url in environment

--- logbook-frontend/src/environments/environment.prod.ts
+++ logbook-frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  baseUrl: 'http://192.168.1.121:8080/0,
+  baseUrl: 'http://192.168.1.121:8080/',
 
   workoutLocations: ['Wilderswil', 'Griffbar', 'K44', 'O\'Bloc', 'Magnet', 'NLZ Biel', 'Bimano', 'Klettertreff', 'Home'],
 };
