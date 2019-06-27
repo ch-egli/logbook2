b16b82db3fe20b590b1661562d34f918e26106a9
ch-egli <chrsitian.egli@gmx.net>
2019-06-27 15:52:34 +0200

    statistics: join status table... not working if there is a different entry for the same date in both tables -> bouldern, trainings are counted double...

--- logbook-service/src/main/java/ch/egli/training/repository/StatisticsRepository.java
+++ logbook-service/src/main/java/ch/egli/training/repository/StatisticsRepository.java
@@ -29,32 +29,37 @@ public class StatisticsRepository {
 
     public List<StatsData> getStatsByUserAndYear(String user, int year) {
         List<StatsData> result;
-        result = jdbcTemplate.query("SELECT date_part('year', datum::date) AS year,\n" +
-                "date_part('week', datum::date) AS week,\n" +
-                "to_date(concat(date_part('year', datum::date), date_part('week', datum::date)), 'iyyyiw') + 3 AS weekDate,\n" +
-                "COUNT(benutzername) AS countTrainings,\n" +
-                "ROUND(AVG(belastung), 1) as avgBelastung,\n" +
-                "MAX(belastung) as maxBelastung,\n" +
-                "ROUND(AVG(trainingszeit), 1) as avgTrainingszeit,\n" +
-                "SUM(zuege12) + SUM(zuege23) + SUM(zuege34) AS totalZuege,\n" +
-                "SUM(zuege12) as zuege12,\n" +
-                "SUM(zuege23) as zuege23,\n" +
-                "SUM(zuege34) as zuege34,\n" +
-                "ROUND(AVG(schlaf),1) AS avgSchlaf,\n" +
-                "SUM(CASE WHEN schlaf < 7 THEN 1 ELSE 0 END) as countSchlafLessThan7,\n" +
-                "ROUND(AVG(gefuehl), 1) as avgGefuehl,\n" +
-                "SUM(CASE WHEN gefuehl > 2 THEN 1 ELSE 0 END) as countGefuehlMoreThan2,\n" +
-                "COUNT(lead) as countLead,\n" +
-                "COUNT(bouldern) as countBouldern,\n" +
-                "COUNT(speed) as countSpeed,\n" +
-                "COUNT(campus) as countCampus,\n" +
-                "COUNT(kraftraum) as countKraft,\n" +
-                "COUNT(dehnen) as countStretching,\n" +
-                "COUNT(mentaltraining) as countMentaltraining,\n" +
-                "COUNT(jogging) as countJogging\n" +
-                "FROM workout w where benutzername = ? and date_part('year', datum::date) = ? \n" +
+        result = jdbcTemplate.query("SELECT \n" +
+                "date_part('year', s.datum::date) AS year,\n" +
+                "date_part('week', s.datum::date) AS week,\n" +
+                "to_date(concat(date_part('year', s.datum::date), date_part('week', s.datum::date)), 'iyyyiw') + 3 AS weekDate,\n" +
+                "COUNT(w.benutzername) AS countTrainings,\n" +
+                "ROUND(AVG(w.belastung), 1) as avgBelastung,\n" +
+                "MAX(w.belastung) as maxBelastung,\n" +
+                "ROUND(AVG(w.trainingszeit), 1) as avgTrainingszeit,\n" +
+                "SUM(w.zuege12) + SUM(w.zuege23) + SUM(w.zuege34) AS totalZuege,\n" +
+                "SUM(w.zuege12) as zuege12,\n" +
+                "SUM(w.zuege23) as zuege23,\n" +
+                "SUM(w.zuege34) as zuege34,\n" +
+                "ROUND(AVG(s.schlaf),1) AS avgSchlaf,\n" +
+                "SUM(CASE WHEN s.schlaf < 7 THEN 1 ELSE 0 END) as countSchlafLessThan7,\n" +
+                "ROUND(AVG(s.gefuehl), 1) as avgGefuehl,\n" +
+                "SUM(CASE WHEN s.gefuehl > 2 THEN 1 ELSE 0 END) as countGefuehlMoreThan2,\n" +
+                "COUNT(w.lead) as countLead,\n" +
+                "COUNT(w.bouldern) as countBouldern,\n" +
+                "COUNT(w.speed) as countSpeed,\n" +
+                "COUNT(w.campus) as countCampus,\n" +
+                "COUNT(w.kraftraum) as countKraft,\n" +
+                "COUNT(w.dehnen) as countStretching,\n" +
+                "COUNT(w.mentaltraining) as countMentaltraining,\n" +
+                "COUNT(w.jogging) as countJogging\n" +
+                "FROM workout w\n" +
+                "  FULL OUTER JOIN status s\n" +
+                "  ON s.datum = w.datum \n" +
+                "WHERE (w.benutzername = ? AND s.benutzername = ?) \n" +
+                "  AND date_part('year', s.datum::date) = ? \n" +
                 "GROUP BY year, week\n" +
-                "ORDER BY year, week", new Object[]{user, year}, new StatisticsRowMapper());
+                "ORDER BY year, week", new Object[]{user, user, year}, new StatisticsRowMapper());
         return result;
     }
 
