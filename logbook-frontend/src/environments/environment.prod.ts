d571b0f5163222b573bc84b8cf2a9794b3742ebc
ch-egli <christian.egli@gmx.net>
2021-02-27 14:58:25 +0100

    Version 2.1.4

--- logbook-frontend/src/environments/environment.prod.ts
+++ logbook-frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   // baseUrl: 'http://192.168.1.121:8080/',
   baseUrl: 'https://logbook.snoopfish.ch:443/',
-  logbookVersion: '2.1.3',
+  logbookVersion: '2.1.4',
 
   workoutLocations: ['Wilderswil', 'Griffbar', 'K44', 'O\'Bloc', 'Magnet', 'Home', 'NLZ Biel', 'Bimano', 'Klettertreff'],
 };
