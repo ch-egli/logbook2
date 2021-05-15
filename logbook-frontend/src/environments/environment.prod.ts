89e11c5ae3198376062b55f7282fbc8e2424e4dc
ch-egli <christian.egli@gmx.net>
2021-05-15 18:24:59 +0200

    Version 2.1.6 Update docker file to include cron

--- logbook-frontend/src/environments/environment.prod.ts
+++ logbook-frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   // baseUrl: 'http://192.168.1.121:8080/',
   baseUrl: 'https://logbook.snoopfish.ch:443/',
-  logbookVersion: '2.1.5',
+  logbookVersion: '2.1.6',
 
   workoutLocations: ['Wilderswil', 'Griffbar', 'K44', 'O\'Bloc', 'Magnet', 'Home', 'NLZ Biel', 'Bimano', 'Klettertreff'],
 };
