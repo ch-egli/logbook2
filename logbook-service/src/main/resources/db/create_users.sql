f09a2036ea4b6edb350ba930fa1e0d7317e01a70
ch-egli <christian.egli@gmx.net>
2022-04-26 19:02:33 +0200

    add lars holzgang

--- logbook-service/src/main/resources/db/create_users.sql
+++ logbook-service/src/main/resources/db/create_users.sql
@@ -1,5 +1,5 @@
 -- benutzer for logbook application
--- password is stored as a bcrypt hash calculated by https://www.dailycred.com/article/bcrypt-calculator
+-- password is stored as a bcrypt hash calculated by https://www.bcryptcalculator.com/encode
 
 INSERT INTO benutzer VALUES(default, 'zoe', '$2a$04$alCNYl6zQ012IcdtlcY3DuifdmXiAOQZXJY7.IgSc50YG8sLZytO2', 'athlet,egliSisters', 'Zoe', 'Egli', null, null, null);  -- 94
 INSERT INTO benutzer VALUES(default, 'liv', '$2a$04$2etITmFEzkCZqPjgxb4JHOOUwDvTZStsjpYGM0ep0qDzrMznBSYZK', 'athlet,egliSisters', 'Liv', 'Egli', null, null, null);  -- 97
@@ -14,4 +14,6 @@ INSERT INTO benutzer VALUES(default, 'francesca', '$2a$04$a4Kn3rpFhqqPcBuutSVTcu
 INSERT INTO benutzer VALUES(default, 'leon', '$2a$04$7Yb1F7PhkP3akJQrjr9GXOWVjlWf9ggu/ocAn/.2sR1pktuVw27zO', 'athlet', 'Leon', 'Niederberger', null, null, null);  -- 17
 INSERT INTO benutzer VALUES(default, 'levin', '$2a$04$Q6xRu1w/0vCyf7TDaexc0.y3UbH9yPj69yI9gIbBAI3YCHmMv9JDa', 'athlet', 'Levin', 'Straubhaar', null, null, null);  -- 23
 
-INSERT INTO benutzer VALUES(default, 'loic', '$2a$10$QB4jieasC1ciiEOGAL2zcuZec7djB.LXiwIiwEaGZuG6BlYpX.lPK', 'trainer', 'Loic', 'Gaidioz', 'lgaidioz@gmail.com ', null, null);  -- 99
+INSERT INTO benutzer VALUES(default, 'lars', '$2a$10$SlapoKW4BndvkItrTWfbVeTQnFnt5hAmW4Khu1rDKEW0HXmpmQ8GW', 'athlet', 'Lars', 'Holzgang', null, null, null);  -- 08
+
+-- INSERT INTO benutzer VALUES(default, 'loic', '$2a$10$QB4jieasC1ciiEOGAL2zcuZec7djB.LXiwIiwEaGZuG6BlYpX.lPK', 'trainer', 'Loic', 'Gaidioz', 'lgaidioz@gmail.com ', null, null);  -- 99
